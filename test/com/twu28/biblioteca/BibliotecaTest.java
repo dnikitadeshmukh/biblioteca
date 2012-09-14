feec8b3724dbb6c8862230784f69442e7030c52f
dnikitadeshmukh <dnikitadeshmukh@gmail.com>
2012-09-14 21:03:09 +0530

    Requirements for Biblioteca code

--- test/com/twu28/biblioteca/BibliotecaTest.java
+++ test/com/twu28/biblioteca/BibliotecaTest.java
@@ -1,11 +1,25 @@
 import org.junit.Test;
-import org.junit.Assert.*;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
+import java.io.IOException;
 
 public class BiblieotecaTests {
     @Test
     public void isAppStartupWelcome(){
         Assert.assertEquals("Welcome", new Biblioteca().startupMessage());
+    }
+
+    @Test
+    public void isMenuPresent(){
+        Assert.assertEquals(true, new Biblioteca().displayMenu());
+    }
+
+    @Test
+    public void validOptionShouldBe1or2or3() throws IOException {
+        Assert.assertTrue(new Biblioteca().isValidOption());
+    }
+
+    @Test
+    public void isReserved() throws IOException {
+        Assert.assertEquals(true, new Biblioteca().reserveBook());
     }
 }
